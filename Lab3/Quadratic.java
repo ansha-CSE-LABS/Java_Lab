f059457e9b4adf2002f4b992c85d79b33a969790
ansha001 <97624380+ansha001@users.noreply.github.com>
2022-01-14 10:38:33 +0530

    Update Quadratic.java

--- Lab3/Quadratic.java
+++ Lab3/Quadratic.java
@@ -1,3 +1,7 @@
+/*Develop a Java program that prints all real solutions to the quadratic equation ax2 +bx+c = 0.
+Read in a, b, c and use the quadratic formula. If the discriminate b2-4ac is negative, display a message stating that there are no real solutions.*/
+    
+    
 import java.lang.*;
 import java.util.*;
 
