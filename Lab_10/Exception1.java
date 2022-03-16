56bea4e7773f4533bc878dc0ce02e2383923af1a
ansha001 <97624380+ansha001@users.noreply.github.com>
2022-03-16 23:49:43 +0530

    Create Exception1.java

---
+++ Lab_10/Exception1.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class WrongAge extends Exception {
+    public String toString() {
+        return " Son's age can not be equal to father's age\n";
+    }
+}
+
+class Father {
+    int fatherAge;
+
+    Father(int fatherAge) {
+        this.fatherAge = fatherAge;
+    }
+}
+
+class Son extends Father {
+    int sonAge;
+
+    Son(int fatherAge, int sonAge) {
+        super(fatherAge);
+        this.sonAge = sonAge;
+    }
+
+    void checkAge() throws WrongAge {
+        if (fatherAge == sonAge) {
+            throw new WrongAge();
+        }
+    }
+}
+
+public class Exception1 {
+    public static void main(String[] args)
+    {
+        Son s1 = new Son (50,15);
+        Son s2 = new Son (40,40);
+        try{
+            s1.checkAge();
+            System.out.println("Acceptable ages");
+        }
+        catch(WrongAge a){
+            System.out.println("Caught exception:"+a);
+        }
+        try{
+            s2.checkAge();
+            System.out.println("Acceptable ages");
+        }
+        catch(WrongAge a){
+            System.out.println("Caught exception:"+a);
+        }
+    }
+}
