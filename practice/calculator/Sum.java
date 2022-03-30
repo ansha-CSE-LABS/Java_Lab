879158ec54ee03c2f57f97c49fe676ae64a65913
ansha001 <97624380+ansha001@users.noreply.github.com>
2022-03-30 09:43:49 +0530

    Add files via upload

---
+++ practice/calculator/Sum.java
@@ -0,0 +1,16 @@
+package calculator;
+import java.util.*;
+
+public class Sum{
+	int a;
+	int b;
+	public Sum (int a, int b){
+		this.a=a;
+		this.b=b;
+	}
+	public void sum (){
+		System.out.println("Sum:"+ (a+b));
+	}
+}
+		
+		
